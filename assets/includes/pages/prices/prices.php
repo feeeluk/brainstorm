b0a51933a6640445e85d82cda2875586b05a58ee
Philip Henning <philiphenning@outlook.com>
2025-07-10 13:50:30 +0100

    Create main pricing section

--- assets/includes/pages/prices/prices.php
+++ assets/includes/pages/prices/prices.php
@@ -6,11 +6,86 @@
 
         <div>
 
-            <p>
-                        
-                This is the general prices section.
+            <table class="prices">
 
+                <colgroup class="table_left"></colgroup>
+                <colgroup span="2" class="table_right"></colgroup>
+
+                <tb>
+
+                    <tr class="title">
+                        <th colspan="3">RATES</th>
+                    </tr>
+                    
+                    <tr class="sub-title">
+                        <td class="hidden"></td>
+                        <td>Ad-hoc</td>
+                        <td>Block</td>
+                    </tr>
+
+                    <tr>
+                        <td class="item">Initial Consultation</td>
+                        <td>FREE</td>
+                        <td>X</td>
+                    </tr>
+                    
+                    <tr>
+                        <td class="item">1 Hour</td>
+                        <td>£59</td>
+                        <td>X</td>
+                    </tr>
+                    
+                    <tr>
+                        <td class="item">1/2 Day</td>
+                        <td>199</td>
+                        <td>X</td>
+                    </tr>
+                    
+                    <tr>
+                        <td class="item">1 Day</td>
+                        <td>£319</td>
+                        <td>X</td>
+                    </tr>
+                    
+                    <tr>
+                        <td class="item">5 Days</td>
+                        <td>X</td>
+                        <td>£1,499</td>
+                    </tr>
+                    
+                    <tr>
+                        <td class="item">10 Days</td>
+                        <td>X</td>
+                        <td>£2,899</td>
+                    </tr>
+                    
+                    <tr>
+                        <td class="item">15-20 Days</td>
+                        <td>X</td>
+                        <td>£4,199</td>
+                    </tr>
+
+                    <tr>
+                        <td class="item">20+ Days (reduced day rate)</td>
+                        <td>£260</td>
+                        <td>X</td>
+                    </tr>
+
+                </tb>
+                
+            </table>
+            
+            <p>
+                Some services are not as 'time-boxable' as others, and therefore not as easy to block book in advance, e.g. Project Management services, or when creating a Proof of Concept solution.
+                In these situations we are happy to bill time ad-hoc, but still within the bounds of this framework.
+                <br>
+                For more details please refer to each service's respective pricing section.
             </p>
+
+            <p>
+                We can of course tailor prices 'off-menu' - which we can discuss in our initial consultation - however this framework still serves as a useful guide.
+            </p>
+
             
         </div>
 
