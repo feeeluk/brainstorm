ffd5a60191d839701f92c2f1f011262de5b7cbef
Philip Henning <philiphenning@outlook.com>
2025-07-20 11:28:52 +0100

    Fix homepage images not being responsive

--- assets/includes/pages/about/about.php
+++ assets/includes/pages/about/about.php
@@ -1,148 +1,104 @@
-<article class="about">
+<article class="about first">
 
     <section class="left">
 
-        <div class="left">
-
-            <h1>ABOUT</h1>
-                
-            <p>
-                We help businesses make better use of their data, tools, and ideas.
-            </p>
-
-            <p>
-               Not every business has the time, headcount, or know-how to take on digital projects in-house.
-            </p>
-
-            <p>
-                That’s where we come in. We work with organizations of all sizes to help them with things like:
-            </p>
-
-            <ul>
-                <li>unlocking insights from existing data</li>
-                <li>managing complex or messy projects</li>
-                <li>turning ideas into working proof-of-concepts</li>
-                <li>training teams on how to use their tools more effectively</li>
-            </ul>
+        <h1>ABOUT</h1>
             
-            <p>
-                We have a mix of technical skill, business sense, and a practical mindset — no jargon, no fluff, just focused support to help move things forward.
-            </p>
-            
-            <p>
-                Whether you're working through a tricky challenge or just need an extra brain on a project, we’re here to help you get where you’re going.
-            </p>
+        <p>
+            We help businesses make better use of their data, tools, and ideas.
+        </p>
 
-            <p>  
-                We are based in Hull (East Yorkshire) but have national scope, so wherever you are in the UK, please feel free to give us a call to discuss how we can help you.
-            </p>
+        <p>
+            Not every business has the time, headcount, or know-how to take on digital projects in-house.
+        </p>
 
-        </div>
+        <p>
+            That’s where we come in.
+            We work with organizations of all sizes to help them with things like:
+        </p>
+
+        <ul>
+            <li>unlocking insights from existing data</li>
+            <li>managing complex or messy projects</li>
+            <li>turning ideas into working proof-of-concepts</li>
+            <li>training teams on how to use their tools more effectively</li>
+        </ul>
+        
+        <p>
+            We have a mix of technical skill, business sense, and a practical mindset — no jargon, no fluff, just focused support to help move things forward.
+        </p>
+        
+        <p>
+            Whether you're working through a tricky challenge or just need an extra brain on a project, we’re here to help you get where you’re going.
+        </p>
+
+        <p>  
+            We are based in Hull (East Yorkshire) but have national scope, so wherever you are in the UK, please feel free to give us a call to discuss how we can help you.
+        </p>
 
     </section>
 
     <section class="right">
 
-        <div class="right">
-
-            <div class="box">
-
+        <div>
+            <span>
                 <a href="/pages/services/proof_of_concept.php">
-
-                    <img style="padding-right: 0px; padding-bottom: 0px;" src="/assets/images/services/proof_of_concept/link_proof_of_concept.webp" alt="Proof of concept">
-                
+                    <img src="/assets/images/services/proof_of_concept/link_proof_of_concept.webp" alt="Proof of concept">
                     <h3>Proof of Concept</h3>
                 </a>
+            </span>
 
-            </div>
-
-            <div class="box">
-
+            <span>
                 <a href="/pages/services/business_analysis.php">
-
-                    <img style="padding-right: 0px; padding-bottom: 0px;" src="/assets/images/services/business_analysis/link_business_analysis.webp" alt="Business analysis">
-                
+                    <img src="/assets/images/services/business_analysis/link_business_analysis.webp" alt="Business analysis">
                     <h3>Business Analysis</h3>
                 </a>
+            </span>
 
-            </div>
-
-            <div class="box">
-
+            <span>
                 <a href="/pages/services/project_management.php">
-
-                    <img style="padding-right: 0px; padding-bottom: 0px;" src="/assets/images/services/project_management/link_project_management.webp" alt="Project managment">
-                
+                    <img src="/assets/images/services/project_management/link_project_management.webp" alt="Project managment">
                     <h3>Project Management</h3>
-
                 </a>
+            </span>
 
-            </div>
-            
-            <div class="box">
-                
-                <a href="/pages/services/software_development.php">
-                    
-                    <img style="padding-right: 0px; padding-bottom: 0px;" src="/assets/images/services/software_development/link_software_development.webp" alt="Software development">
-                    
+            <span>
+                <a href="/pages/services/software_development.php">            
+                    <img src="/assets/images/services/software_development/link_software_development.webp" alt="Software development">                     
                     <h3>Software Development</h3>
-                    
                 </a>
-                
-            </div>
+            </span>
 
-            <div class="box">
-
+            <span>
                 <a href="/pages/services/data_management.php">
-
-                    <img style="padding-right: 0px; padding-bottom: 0px;" src="/assets/images/services/data_management/link_data_management.webp" alt="Data management">
-                                    
+                    <img src="/assets/images/services/data_management/link_data_management.webp" alt="Data management">
                     <h3>Data Management</h3>
+                </a>
+            </span>
 
-                </a>     
-
-            </div>
-
-            
-
-            <div class="box">
-
+            <span>
                 <a href="/pages/services/skills_development_and_training.php">
-
-                    <img style="padding-right: 0px; padding-bottom: 0px;" src="/assets/images/services/skills_development_and_training/link_skills_development_and_training.webp" alt="Skills development & training">
-                
+                    <img src="/assets/images/services/skills_development_and_training/link_skills_development_and_training.webp" alt="Skills development & training">
                     <h3>Skills Development & Training</h3>
-
                 </a>
+            </span>
 
-            </div>
-
-            <div class="box">
-
+            <span>
                 <a href="/pages/services/artificial_intelligence.php">
-
                     <img src="/assets/images/services/artificial_intelligence/link_artificial_intelligence.webp" alt="Artificial intelligence">
-                
                     <h3>Artificial Intelligence</h3>
-
                 </a>
+            </span>
 
-            </div>
-            
-            <div class="box">
-                
+            <span>
                 <a href="/pages/services/digital_media_and_marketing.php">
-
-                    <img src="assets/images/services/digital_media_and_marketing/link_digital_media_and_marketing.webp" alt="Digital media and marketing">
-                
+                    <img src="/assets/images/services/digital_media_and_marketing/link_digital_media_and_marketing.webp" alt="Digital media and marketing" alt="Data management">
                     <h3>Digital Media & Marketing</h3>
-
                 </a>
-
-            </div>
-
+            </span>
         </div>
 
     </section>
 
 </article>
+
